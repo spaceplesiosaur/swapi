331bc79207b7c9144c814f649084e5517e61f6fa
Raisa Primerova <riyuhide@gmail.com>
2019-12-04 09:00:37 -0700

    Add LoginInput functionality and styles

---
+++ src/components/login/LoginInput/LoginInput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './LoginInput.scss'
+
+const LoginInput = ({type, value, toggleChangeState}) => {
+  const handleChange = (event) => {
+    toggleChangeState(type, event.target.value)
+  }
+
+  return (
+    <section className="login-input">
+      <label htmlFor={type.replace(' ', '-')}>{`your ${type}`}</label>
+      <input
+        id={type.replace(' ', '-')}
+        value={value}
+        placeholder={`enter your ${type}`}
+        onChange={handleChange}/>
+    </section>
+  )
+}
+
+export default LoginInput
