dc0597c42251c888d37aa80970e638aa73de03bd
Allie McCarthy <mccarthy.allie@gmail.com>
2019-12-08 02:23:19 -0700

    fix fake data

--- src/mockData/fakeCharacters.js
+++ src/mockData/fakeCharacters.js
@@ -1,9 +1,9 @@
 const characterResults = [
-  {name: "Luke", species: "human", planet: "Tatioone", population: 20000, films: ['bambi', 'little women', 'sound of music']},
-  {name: "Leia", species: "human", planet: "Alderaan", population: 230000, films: ['die hard']},
-  {name: "Obi-wan", species: "human", planet: "Whereever he is from", population: 40000, films: ['lawrence of arabia']},
-  {name: "Darth Vadar", species: "robot", planet: "Tatioone", population: 50000, films: ['punch drunk love']},
-  {name: "Han", species: "human", planet: "Millenium Falcon", population: 60000, films:['cinderella']}
+  {name: "Luke", species: "human", planet: "Tatioone", population: 20000, films: ['bambi', 'little women', 'sound of music'], isFavorite: true},
+  {name: "Leia", species: "human", planet: "Alderaan", population: 230000, films: ['die hard'], isFavorite: true},
+  {name: "Obi-wan", species: "human", planet: "Whereever he is from", population: 40000, films: ['lawrence of arabia'], isFavorite: false},
+  {name: "Darth Vadar", species: "robot", planet: "Tatioone", population: 50000, films: ['punch drunk love'], isFavorite: true},
+  {name: "Han", species: "human", planet: "Millenium Falcon", population: 60000, films:['cinderella'], isFavorite: false}
 ]
 
 export default characterResults;
