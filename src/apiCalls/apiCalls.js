167eea6e628f48d6331f5057bb5c087eab00f8a5
Allie McCarthy <mccarthy.allie@gmail.com>
2019-12-09 16:53:32 -0700

    Refactor fetch calls into one function and move cleanup to App

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,20 +1,3 @@
-export const getMoviesData = () => {
-  return fetch('https://swapi.co/api/films/')
-    .then(response => {
-      if (!response.ok) {
-        throw Error('Movies wasn\'t fetched')
-      }
-      return response.json()
-    })
-    .then(movies => movies.results.sort((a, b) => {
-      return a.episode_id - b.episode_id
-    }))
-    .then(movies => movies.map(movie => {
-      const { title, episode_id, opening_crawl, release_date, characters } = movie
-      return { title, episode_id, opening_crawl, release_date, characters }
-    }))
-}
-
 export const getAnyData = (url, type) => {
   return fetch(url)
     .then(response => {
