6a39cf2c33a0792dd4592ab86c88e9f29382e8b6
Raisa Primerova <riyuhide@gmail.com>
2019-12-30 17:29:08 -0700

    Updates

---
+++ precache-manifest.1487253b6d939d60f362ba71e86f88d6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e215c27a3d79156c7707a23f56c3aa",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "bf699444f7174bebe219",
+    "url": "/swapi/static/css/main.ddec2531.chunk.css"
+  },
+  {
+    "revision": "5104f2b5d27b9f11b550",
+    "url": "/swapi/static/js/2.2d56db04.chunk.js"
+  },
+  {
+    "revision": "bf699444f7174bebe219",
+    "url": "/swapi/static/js/main.231273ff.chunk.js"
+  },
+  {
+    "revision": "b78505a19f97cd5c3ce1",
+    "url": "/swapi/static/js/runtime-main.6715cd0d.js"
+  },
+  {
+    "revision": "021bc124df0cced42a1e85896420e83a",
+    "url": "/swapi/static/media/artoo.021bc124.png"
+  },
+  {
+    "revision": "73ef89546df67fd5173105b97d80395b",
+    "url": "/swapi/static/media/back.73ef8954.svg"
+  },
+  {
+    "revision": "3cd156613adf658bbc9f32e8e12556cd",
+    "url": "/swapi/static/media/bg-image.3cd15661.jpg"
+  },
+  {
+    "revision": "0ba9d8ec28ee807cb01d35adc9fa80d7",
+    "url": "/swapi/static/media/c3po.0ba9d8ec.png"
+  },
+  {
+    "revision": "e0ad04f4584bc700c5749ba419951f63",
+    "url": "/swapi/static/media/c3po.e0ad04f4.svg"
+  },
+  {
+    "revision": "867d908a3ca39a6a4b7e06187a17402c",
+    "url": "/swapi/static/media/death-star.867d908a.svg"
+  },
+  {
+    "revision": "d8f1ad15a7b122e079b8e0b639a5f9df",
+    "url": "/swapi/static/media/full-star.d8f1ad15.svg"
+  },
+  {
+    "revision": "c49307c61cfea123f8ffd9d311d018a8",
+    "url": "/swapi/static/media/hand.c49307c6.svg"
+  },
+  {
+    "revision": "b9edec22fbf7c800590ebfd671467439",
+    "url": "/swapi/static/media/mars.b9edec22.svg"
+  },
+  {
+    "revision": "5ac0ff3e56b4475c48ce4b827f6f99fe",
+    "url": "/swapi/static/media/obiWan.5ac0ff3e.svg"
+  },
+  {
+    "revision": "75b6b2a352b8f41665abcdb5b6d2ca1c",
+    "url": "/swapi/static/media/population.75b6b2a3.svg"
+  },
+  {
+    "revision": "8981c66a7699c0a1ec8c5faa0e34a11a",
+    "url": "/swapi/static/media/space.8981c66a.jpg"
+  },
+  {
+    "revision": "571f0ecd7a686207b0e6367878f9ddaf",
+    "url": "/swapi/static/media/st.571f0ecd.svg"
+  },
+  {
+    "revision": "818c99aaf7c8c31df28324ba43a773d2",
+    "url": "/swapi/static/media/star.818c99aa.svg"
+  },
+  {
+    "revision": "adf6c3058ecc95887537723558d6cfe5",
+    "url": "/swapi/static/media/yoda.adf6c305.png"
+  }
+]);
